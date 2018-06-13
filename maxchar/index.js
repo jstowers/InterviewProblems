97263d0ece90ed05ae6a9adaa6a56ba123a03f8c
Joe Stowers <greenchiletexan@gmail.com>
2018-06-13 13:44:15 -0500

    maxchar: initial commit of index and test files

---
+++ maxchar/index.js
@@ -0,0 +1,23 @@
+// MaxChar
+// Joseph Stowers
+// June 13, 2018
+
+/*
+	Given a string, return the character that appears the most within the string.
+*/
+
+function maxChar(str) {
+
+	// split the string into characters
+	let maxCharacter = '';
+
+	// begin looping through the string
+
+	// create an object that stores the character and the count { char: count } 	
+
+
+	// return the character with the highest count
+	return maxCharacter;
+}
+
+module.exports = maxChar;
---
+++ maxchar/test.js
@@ -0,0 +1,14 @@
+const maxChar = require('./index');
+
+test('maxChar function is defined', () => {
+	expect(typeof maxChar).toEqual('function');
+});
+
+test('returns a character', () => {
+	expect(typeof maxChar('test')).toEqual('string');
+});
+
+test('returns \'t\' for string \'test\'', () => {
+  	expect(maxChar('test')).toEqual('t');
+});
+
