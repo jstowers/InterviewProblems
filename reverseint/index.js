31e7f239ae0fe8700c3ec5e71abd819b44c48beb
Joe Stowers <greenchiletexan@gmail.com>
2018-06-19 09:11:16 -0500

    reverseint: refactor index and test files for clarity

--- reverseint/index.js
+++ reverseint/index.js
@@ -26,15 +26,14 @@ function reverseInt(n) {
 
 	// convert int to string and split into array
 	let stringArr = n.toString().split('');
-	console.log("stringArr = ", stringArr);
-
+	
 	// create storage array
 	let storage = [];
 
 	// loop through string from right to left
 	// if digit != 0 || storage.length > 0, then push digit into storage array
 	for (let i = stringArr.length - 1; i >= 0; i-- ) {
-		console.log('stringArr[i] = ', stringArr[i]);
+		// console.log('stringArr[i] = ', stringArr[i]);
 
 		if (stringArr.length === 1 && stringArr[i] == 0) {
 			return 0;
@@ -45,23 +44,10 @@ function reverseInt(n) {
 
 	// convert string to integer and check for sign
 	let result = parseInt(storage.join('')) * sign;
-	console.log('result = ', result);
-	console.log('result is an integer =', Number.isInteger(result));
+	// console.log('result = ', result);
+	// console.log('result is an integer =', Number.isInteger(result));
 
 	return result;
 }
-
-
-console.log('reverseInt(15) =', reverseInt(15));
-
-console.log('reverseInt(900) =', reverseInt(900));
-
-console.log('reverseInt(0) =', reverseInt(0));
-
-console.log('reverseInt(-90) =', reverseInt(-90));
-
-console.log('reverseInt(-19) =', reverseInt(-19));
-
-console.log('reverseInt(-5) =', reverseInt(-5));
 
 module.exports = reverseInt;
--- reverseint/test.js
+++ reverseint/test.js
@@ -7,9 +7,23 @@
 const reverseInt = require('./index');
 
 test('reverseInt function is defined', () => {
-  expect(reverseInt).toBeDefined();
+  	expect(reverseInt).toBeDefined();
 });
 
 test('reverseInt handles 0 as an input', () => {
 	expect(reverseInt(0)).toEqual(0);
+});
+
+test('reverseInt flips a positive number', () => {
+	expect(reverseInt(5)).toEqual(5);
+	expect(reverseInt(15)).toEqual(51);
+	expect(reverseInt(90)).toEqual(9);
+	expect(reverseInt(2359)).toEqual(9532);
+});
+
+test('reverseInt flips a negative number', () => {
+	expect(reverseInt(-5)).toEqual(-5);
+	expect(reverseInt(-15)).toEqual(-51);
+	expect(reverseInt(-90)).toEqual(-9);
+	expect(reverseInt(-2359)).toEqual(-9532);
 });
