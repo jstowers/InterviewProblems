4f57b6be2264208cfe28397392c502f1edc298d1
Joe Stowers <greenchiletexan@gmail.com>
2019-05-04 11:23:34 -0500

    events: complete eventing library to register events

--- events/index.js
+++ events/index.js
@@ -3,17 +3,46 @@
 // Events class.  The Events class should
 // have methods 'on', 'trigger', and 'off'.
 
+// Events Library
+
+// 1. call functon with name of event,
+// 2. invokes a callback that performs an operation
+
 class Events {
-    // Register an event handler
-    on(eventName, callback) {}
+
+    constructor() {
+        this.events = {}
+        // keys = eventNames
+        // values = array of callback functions
+    }
+
+    // Register an event handler with a given name
+    on(eventName, callback) {
+        if(this.events[eventName]) {
+            this.events[eventName].push(callback);
+        }
+        else {
+            this.events[eventName] = [callback];
+        }
+    }
   
-    // Trigger all callbacks associated
-    // with a given eventName
-    trigger(eventName) {}
+    // Immediately trigger all callbacks associated with a given eventName 
+    trigger(eventName) {
+        if(this.events[eventName]) {
+            let arrayOfCallbacks = this.events[eventName];
+            for(let i = 0; i < arrayOfCallbacks.length; i++) {
+                arrayOfCallbacks[i]();
+            }
+        }
+        return null;
+    }
   
-    // Remove all event handlers associated
-    // with the given eventName
-    off(eventName) {}
+    // Remove all event handlers associated with the given eventName
+    off(eventName) {
+        if(this.events[eventName]) {
+            this.events[eventName] = [];
+        }
+    }
   }
   
   module.exports = Events;
